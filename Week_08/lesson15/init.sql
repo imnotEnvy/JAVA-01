1b8b93375f3158878adefe0c1c0304812debfc29
L3nvy <l3nvyr@163.com>
2021-03-14 04:17:01 +0800

    update: [WEEK-08] upload 15-2

---
+++ Week_08/lesson15/init.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS demo_ds_0;
+CREATE DATABASE IF NOT EXISTS demo_ds_1;
+
+CREATE TABLE IF NOT EXISTS demo_ds_0.t_order_0 (user_id bigint, status varchar(10), order_id bigint);
+CREATE TABLE IF NOT EXISTS demo_ds_0.t_order_1 (user_id bigint, status varchar(10), order_id bigint);
+CREATE TABLE IF NOT EXISTS demo_ds_0.t_order_2 (user_id bigint, status varchar(10), order_id bigint);
+CREATE TABLE IF NOT EXISTS demo_ds_0.t_order_3 (user_id bigint, status varchar(10), order_id bigint);
+CREATE TABLE IF NOT EXISTS demo_ds_0.t_order_4 (user_id bigint, status varchar(10), order_id bigint);
+CREATE TABLE IF NOT EXISTS demo_ds_0.t_order_5 (user_id bigint, status varchar(10), order_id bigint);
+CREATE TABLE IF NOT EXISTS demo_ds_0.t_order_6 (user_id bigint, status varchar(10), order_id bigint);
+CREATE TABLE IF NOT EXISTS demo_ds_0.t_order_7 (user_id bigint, status varchar(10), order_id bigint);
+CREATE TABLE IF NOT EXISTS demo_ds_1.t_order_0 (user_id bigint, status varchar(10), order_id bigint);
+CREATE TABLE IF NOT EXISTS demo_ds_1.t_order_1 (user_id bigint, status varchar(10), order_id bigint);
+CREATE TABLE IF NOT EXISTS demo_ds_1.t_order_2 (user_id bigint, status varchar(10), order_id bigint);
+CREATE TABLE IF NOT EXISTS demo_ds_1.t_order_3 (user_id bigint, status varchar(10), order_id bigint);
+CREATE TABLE IF NOT EXISTS demo_ds_1.t_order_4 (user_id bigint, status varchar(10), order_id bigint);
+CREATE TABLE IF NOT EXISTS demo_ds_1.t_order_5 (user_id bigint, status varchar(10), order_id bigint);
+CREATE TABLE IF NOT EXISTS demo_ds_1.t_order_6 (user_id bigint, status varchar(10), order_id bigint);
+CREATE TABLE IF NOT EXISTS demo_ds_1.t_order_7 (user_id bigint, status varchar(10), order_id bigint);
